c3cc721e60498ede858936667aae90915f01ee95
Adrian <alextravc.342@gmail.com>
2023-10-27 13:10:31 -0500

    [ADD] UserMapper

---
+++ lib/features/auth/infrastructure/mappers/user_mapper.dart
@@ -0,0 +1,18 @@
+import 'package:teslo_shop/features/auth/domain/domain.dart';
+
+
+class UserMapper {
+
+  static User userJsonToEntity(Map<String, dynamic> json) => User(
+    id: json['id'],
+    email: json['email'],
+    fullName: json['fullName'],
+    isActive: json['isActive'],
+    token: json['token'],
+
+    // mapear el Array q nos retorna la api: {roles:['admin']}
+    roles: List<String>.from(json['roles'].map((role) => role)),
+
+  );
+
+}
