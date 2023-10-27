4ca4bf4492cc60910961d04d1067992b51e91a22
Adrian <alextravc.342@gmail.com>
2023-10-27 13:10:31 -0500

    [ADD] AuthProvider initial setup

---
+++ lib/features/auth/providers/auth_provider.dart
@@ -0,0 +1,86 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
+
+import '../domain/domain.dart';
+
+
+
+// // // StateNotifierProvider to be used with StateNotifier
+final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
+
+  final authRepository = AuthRepositoryImpl(datasource: AuthDatasourceImpl());
+
+  return AuthNotifier(authRepository: authRepository);
+});
+
+
+
+
+// // // State: Con Riverpod NO es indispensable el Equatable para comparar states como en Bloc
+enum AuthStatus { checking, authenticated, notAuthenticated }
+
+class AuthState {
+  final AuthStatus authStatus;
+  final User? user;
+  final String errorMessage;
+
+  AuthState({
+    this.authStatus = AuthStatus.checking,
+    this.user,
+    this.errorMessage = '',
+  });
+
+
+  // // keep it inmutable: create new state based on previous one
+  AuthState copyWith({
+    AuthStatus? authStatus,
+    User? user,
+    String? errorMessage,
+  }) => AuthState(
+    authStatus: authStatus ?? this.authStatus,
+    user: user ?? this.user,
+    errorMessage: errorMessage ?? this.errorMessage,
+  );
+
+}
+
+
+
+// // // State notifier: Generico para varios Providers de != useCases
+class AuthNotifier extends StateNotifier<AuthState> {
+
+  // de 1 el repo, sin use cases individuales con sus providers
+  final AuthRepository authRepository;
+
+  AuthNotifier({
+    required this.authRepository
+  }): super(AuthState());
+
+
+  void login(String email, String password) async {
+    // fake a slightly slow request
+    await Future.delayed(const Duration(milliseconds: 300));
+
+    // final user = await authRepository.login(email, password);
+
+    // // create new state based on prev to notify to everyone (re-render)
+    // state.copyWith(
+    //   user: user,
+    // );
+  }
+
+  void register(String email, String password) {
+
+  }
+
+  void checkAuthStatus(String email, String password) {
+
+  }
+
+  // Future<void> logout() {
+
+  // }
+
+}
+
+
