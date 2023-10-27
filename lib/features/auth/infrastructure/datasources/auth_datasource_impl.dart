00b2a2d64366b13220e74c96dd5f21b930fa1086
Adrian <alextravc.342@gmail.com>
2023-10-27 13:10:31 -0500

    [ADD] AuthDatasourceImpl login method + dio setup

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -1,7 +1,18 @@
+import 'package:dio/dio.dart';
+
+import 'package:teslo_shop/config/config.dart';
 import 'package:teslo_shop/features/auth/domain/domain.dart';
+import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
 
 
 class AuthDatasourceImpl extends AuthDatasource {
+
+  // // dio para este datasource
+  final dio = Dio(BaseOptions(
+    baseUrl: Environment.apiUrl,
+
+  ));
+
 
   @override
   Future<User> checkAuthStatus(String token) {
@@ -9,8 +20,22 @@ class AuthDatasourceImpl extends AuthDatasource {
   }
 
   @override
-  Future<User> login(String email, String password) {
-    throw UnimplementedError();
+  Future<User> login(String email, String password) async {
+
+    try {
+      final res = await dio.post('/auth/login', data: {
+        'email': email,
+        'password': password,
+      });
+
+      // mapper
+      final user = UserMapper.userJsonToEntity(res.data);
+
+      return user;
+    } catch (e) {
+      throw WrongCredentials();
+    }
+
   }
 
   @override
