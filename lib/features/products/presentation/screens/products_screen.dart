acde733c225103e3235044521a33ac28f0e5f452
Adrian <alextravc.342@gmail.com>
2023-10-28 14:06:17 -0500

    [ADD] initial loadNextPage in initState of ProductScreen

--- lib/features/products/presentation/screens/products_screen.dart
+++ lib/features/products/presentation/screens/products_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+import 'package:teslo_shop/features/products/presentation/providers/products_provider.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
+
 
 
 class ProductsScreen extends StatelessWidget {
@@ -36,11 +41,60 @@ class ProductsScreen extends StatelessWidget {
 }
 
 
-class _ProductsView extends StatelessWidget {
+
+// // InfiniteScroll requires handle State | ConsumerStatefulWidget
+class _ProductsView extends ConsumerStatefulWidget {
   const _ProductsView();
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('Eres genial!'));
+  _ProductsViewState createState() => _ProductsViewState();
+}
+
+
+class _ProductsViewState extends ConsumerState<_ProductsView> { // ref is global
+
+  // infinite scroll
+  final ScrollController scrollController = ScrollController();
+
+
+  @override
+  void initState() { // ngOnInit() :v
+    super.initState(); // always first
+
+    // inicia la http req con toda la clean arch: init fetch products
+    ref.read(productsProvider.notifier).loadNextPage();
+
+    // // // infinite scroll
+
   }
+  @override
+  void dispose() {
+    super.dispose();
+    scrollController.dispose(); // all controller must be cleaned
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    final productsState = ref.watch(productsProvider); // return ProductsState
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: MasonryGridView.count(
+        physics: const BouncingScrollPhysics(), // rebote ios/android
+        crossAxisCount: 2, // # columns
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 35,
+
+        // controller: scrollController, // infinite scroll
+
+        itemCount: productsState.products.length,
+        itemBuilder: (context, index) {
+          final product = productsState.products[index];
+          return Text(product.title);
+        },
+      ),
+    );
+  }
+
 }
